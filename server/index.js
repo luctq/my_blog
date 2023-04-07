97a0f8d909326b78444bd50846ee0b95e9c3dd74
luctq <tranquocluc610@gmail.com>
2023-04-08 01:26:20 +0700

    Backend: set up server and connect to moongo DB

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.listen("5000", () => {
+  console.log("Server is running in port 5000");
+});
