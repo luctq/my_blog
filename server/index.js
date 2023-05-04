3abd9112d448c57fe609f03de285dc63e8e3cbe4
luctq <tranquocluc610@gmail.com>
2023-05-04 23:17:32 +0700

    create api post/get catergories and upload file image by multer

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
+const multer = require("multer")
 
 const express = require("express");
 const app = express();
@@ -24,6 +25,19 @@ mongoose
     console.log(err.message);
   });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "test.jpg");
+  }
+})
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+})
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts",  postRoute);
--- server/routes/categories.js
+++ server/routes/categories.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
     try {
-        const categories = Category.find();
+        const categories = await Category.find();
         res.status(200).json(categories)
     } catch (err) {
         res.status(500).json(err)
